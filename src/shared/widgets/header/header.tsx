943f4be0001fad57d4702b1dd7e2161c5b9cc88e
Aditya <codecommander03@gmail.com>
2024-04-06 21:06:42 +0530

    header v1 with logo done

---
+++ src/shared/widgets/header/header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+import Logo from "./logo";
+
+type Props = {};
+
+const Header = (props: Props) => {
+    return <header className="w-full sticky top-0 left-0 z-[999] border-b border-b[#000] px-10 flex items-center justify-between h-[80px] bg-white text-black">
+        <div>
+            <Link href={"/"}>
+                <Logo/>
+            </Link>
+        </div>
+    </header>
+};
+
+export default Header;
