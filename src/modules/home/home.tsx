91bb1de24e6ecdd91a77984c48669991352d6166
Aditya <codecommander03@gmail.com>
2024-04-07 22:20:22 +0530

    home page components added

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,14 +1,23 @@
 import Header from '@/shared/widgets/header/header'
 import React from 'react'
+import Banner from './features/banner'
+import Branding from './features/branding'
+import Benefits from './benefits'
+import FeatureHighlight from './feature.highlight'
 
 type Props = {}
 
 const Home = (props: Props) => {
   return (
     <div>
-      <Header/>
+      <Header />
+      <Banner />
+      <Branding />
+      <Benefits />
+      {/* FREE trial card pending */}
+      <FeatureHighlight />
     </div>
   )
 }
 
-export default Home
+export default Home;
