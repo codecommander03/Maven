1b772164ba2bc1a4ba15ef4067086f9a89bbfd61
Aditya <codecommander03@gmail.com>
2024-04-17 22:16:41 +0530

    stripe id added

--- src/shared/components/cards/pricing.card.tsx
+++ src/shared/components/cards/pricing.card.tsx
@@ -10,9 +10,9 @@ const PricingCard = ({ active }: { active: string }) => {
     const history = useRouter();
     const handleSubscription = async ({ price }: { price: string }) => {
         await stripeSubscribe({ price: price, userId: user?.id! }).then(
-        (res: any) => {
-            history.push(res);
-        }
+            (res: any) => {
+                history.push(res);
+            }
         );
     };
 
@@ -39,113 +39,113 @@ return (
                 </h5>
                 <br />
                 <div className="border-b pb-8 border-[#000]">
-                <h5 className="font-clashDisplay uppercase text-cyber-ink text-3xl">
-                    $0
-                </h5>
-                <p className="text-lg">No commitment</p>
+                    <h5 className="font-clashDisplay uppercase text-cyber-ink text-3xl">
+                        $0
+                    </h5>
+                    <p className="text-lg">No commitment</p>
                 </div>
                 <div className="pt-5">
-                <p className="text-xl">What&apos;s included...</p>
+                    <p className="text-xl">What&apos;s included...</p>
                 </div>
                 {freePlan.map((i: PlanType, index: number) => (
-                <div key={index} className="flex w-full items-center py-4">
-                    <span className="text-xl">{ICONS.right}</span>
-                    <p className="pl-2 text-lg">{i.title}</p>
-                </div>
+                    <div key={index} className="flex w-full items-center py-4">
+                        <span className="text-xl">{ICONS.right}</span>
+                        <p className="pl-2 text-lg">{i.title}</p>
+                    </div>
                 ))}
                 <br />
                 <Button color="primary" className="w-full text-xl !py-6">
-                Get Started
+                    Get Started
                 </Button>
                 <p className="pt-1 opacity-[.7] text-center">
-                30-day free trial of Scale features, then free forever
+                    30-day free trial of Scale features, then free forever
                 </p>
             </div>
 
             {/* grow plan */}
             <div className="md:w-[400px] bg-white rounded p-5 my-5 md:my-0">
                 <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="36"
-                height="33"
-                fill="string"
-                className="mb-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="36"
+                    height="33"
+                    fill="string"
+                    className="mb-4"
                 >
-                <path
-                    fill="#fff"
-                    stroke="#3843D0"
-                    stroke-width="3"
-                    d="M33.398 13.25a6.512 6.512 0 0 1 0 6.5l-4.887 8.487a6.512 6.512 0 0 1-5.643 3.263h-9.736a6.512 6.512 0 0 1-5.643-3.263L2.602 19.75a6.512 6.512 0 0 1 0-6.498l4.887-8.488A6.512 6.512 0 0 1 13.132 1.5h9.736a6.512 6.512 0 0 1 5.643 3.263l4.887 8.488Z"
-                ></path>
+                    <path
+                        fill="#fff"
+                        stroke="#3843D0"
+                        stroke-width="3"
+                        d="M33.398 13.25a6.512 6.512 0 0 1 0 6.5l-4.887 8.487a6.512 6.512 0 0 1-5.643 3.263h-9.736a6.512 6.512 0 0 1-5.643-3.263L2.602 19.75a6.512 6.512 0 0 1 0-6.498l4.887-8.488A6.512 6.512 0 0 1 13.132 1.5h9.736a6.512 6.512 0 0 1 5.643 3.263l4.887 8.488Z"
+                    ></path>
                 </svg>
                 <h5 className="font-clashDisplay uppercase text-cyber-ink text-3xl pb-8 border-b border-[#000]">
-                GROW
+                    GROW
                 </h5>
                 <br />
                 <div className="border-b pb-8 border-black">
-                <h5 className="font-clashDisplay uppercase text-cyber-ink text-3xl">
-                    ${active === "Monthly" ? "49" : "42"} /month
-                </h5>
-                <p className="text-lg">Billed {active}</p>
+                    <h5 className="font-clashDisplay uppercase text-cyber-ink text-3xl">
+                        ${active === "Monthly" ? "49" : "42"} /month
+                    </h5>
+                    <p className="text-lg">Billed {active}</p>
                 </div>
                 <div className="pt-5">
-                <p className="text-xl">Everything in Launch, plus...</p>
+                    <p className="text-xl">Everything in Launch, plus...</p>
                 </div>
                 {GrowPlan.map((i: PlanType, index: number) => (
-                <div key={index} className="flex w-full items-center py-4">
-                    <span className="text-xl">{ICONS.right}</span>
-                    <p className="pl-2 text-lg">{i.title}</p>
-                </div>
+                    <div key={index} className="flex w-full items-center py-4">
+                        <span className="text-xl">{ICONS.right}</span>
+                        <p className="pl-2 text-lg">{i.title}</p>
+                    </div>
                 ))}
                 <br />
                 <Button
-                color="primary"
-                className="w-full text-xl !py-6"
-                onClick={() =>
-                    handleSubscription({
-                    price:
-                        active === "Monthly"
-                        ? "price_1OnaWFSA1WAzNgKlsGN6K4ZW"
-                        : "price_1Onbt8SA1WAzNgKlyrXYlJBG",
-                    })
-                }
+                    color="primary"
+                    className="w-full text-xl !py-6"
+                    onClick={() =>
+                        handleSubscription({
+                        price:
+                            active === "Monthly"
+                            ? "price_1P6ZLDSBSDfsApmCt0t2JZJX"
+                            : "price_1P6ZaFSBSDfsApmC0JeGg0v1",
+                        })
+                    }
                 >
-                Get Started
+                    Get Started
                 </Button>
                 <p className="pt-1 opacity-[.7] text-center">
-                30-day free trial of Scale features, then $
-                {active === "Monthly" ? "42" : "49"}/mo
+                    30-day free trial of Scale features, then $
+                    {active === "Monthly" ? "42" : "49"}/mo
                 </p>
             </div>
 
             {/* scale plan */}
             <div className="md:w-[400px] bg-white rounded p-5 my-5 md:my-0">
                 <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="36"
-                height="33"
-                fill="string"
-                className="mb-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="36"
+                    height="33"
+                    fill="string"
+                    className="mb-4"
                 >
-                <path
-                    fill="#fff"
-                    stroke="#3843D0"
-                    stroke-width="3"
-                    d="M33.398 13.25a6.512 6.512 0 0 1 0 6.5l-4.887 8.487a6.512 6.512 0 0 1-5.643 3.263h-9.736a6.512 6.512 0 0 1-5.643-3.263L2.602 19.75a6.512 6.512 0 0 1 0-6.498l4.887-8.488A6.512 6.512 0 0 1 13.132 1.5h9.736a6.512 6.512 0 0 1 5.643 3.263l4.887 8.488Z"
-                ></path>
+                    <path
+                        fill="#fff"
+                        stroke="#3843D0"
+                        stroke-width="3"
+                        d="M33.398 13.25a6.512 6.512 0 0 1 0 6.5l-4.887 8.487a6.512 6.512 0 0 1-5.643 3.263h-9.736a6.512 6.512 0 0 1-5.643-3.263L2.602 19.75a6.512 6.512 0 0 1 0-6.498l4.887-8.488A6.512 6.512 0 0 1 13.132 1.5h9.736a6.512 6.512 0 0 1 5.643 3.263l4.887 8.488Z"
+                    ></path>
                 </svg>
                 <h5 className="font-clashDisplay uppercase text-cyber-ink text-3xl pb-8 border-b border-[#000]">
-                SCALE
+                    SCALE
                 </h5>
                 <br />
                 <div className="border-b pb-8 border-[#000]">
-                <h5 className="font-clashDisplay uppercase text-cyber-ink text-3xl">
-                    ${active === "Monthly" ? "99" : "84"} /month
-                </h5>
-                <p className="text-lg">Billed {active}</p>
+                    <h5 className="font-clashDisplay uppercase text-cyber-ink text-3xl">
+                        ${active === "Monthly" ? "99" : "84"} /month
+                    </h5>
+                    <p className="text-lg">Billed {active}</p>
                 </div>
                 <div className="pt-5">
-                <p className="text-xl">Everything in Grow, plus...</p>
+                    <p className="text-xl">Everything in Grow, plus...</p>
                 </div>
                 {scalePlan.map((i: PlanType, index: number) => (
                 <div key={index} className="flex w-full items-center py-4">
@@ -155,22 +155,22 @@ return (
                 ))}
                 <br />
                 <Button
-                color="primary"
-                className="w-full text-xl !py-6"
-                onClick={() =>
-                    handleSubscription({
-                    price:
-                        active === "Monthly"
-                        ? "price_1On2H2SA1WAzNgKlV64Zj6gE"
-                        : "price_1Onf9gSA1WAzNgKlg8NLBP4r",
-                    })
-                }
+                    color="primary"
+                    className="w-full text-xl !py-6"
+                    onClick={() =>
+                        handleSubscription({
+                        price:
+                            active === "Monthly"
+                            ? "price_1P6ZNTSBSDfsApmCLo6VZVMM"
+                            : "price_1P6ZaoSBSDfsApmCuN6HSbiY",
+                        })
+                    }
                 >
-                Get Started
+                    Get Started
                 </Button>
                 <p className="pt-1 opacity-[.7] text-center">
-                30-day free trial of Scale features, then $
-                {active === "Monthly" ? "99" : "84"}/mo
+                    30-day free trial of Scale features, then $
+                    {active === "Monthly" ? "99" : "84"}/month
                 </p>
             </div>
         </div>
